d24f74834feaa3beb4fd64b873e51d63f8c4c6bd
FerRufato <131503375+FerRufato@users.noreply.github.com>
2024-10-05 01:00:00 -0300

    Add files via upload
    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+var resultadoMultiplicacao = multiplicar(4, 7);
+var saudacao = saudar("Maria");
+console.log(resultadoMultiplicacao); // Deve imprimir 28
+console.log(saudacao); // Deve imprimir "Olá Maria"
