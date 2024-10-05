48c91a684d7b69e35e4185a149cbb91163f664da
FerRufato <131503375+FerRufato@users.noreply.github.com>
2024-10-05 01:07:39 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(4, 7);
+const saudacao = saudar("Maria");
+
+console.log(resultadoMultiplicacao); 
+console.log(saudacao); 
